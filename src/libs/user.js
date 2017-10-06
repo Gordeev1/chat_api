703eb2e53cf9470afd7196fa52000815c9ba768a
Artem Gordeev <gordeevartem.dev@gmail.com>
2017-10-06 17:15:04 +0300

    fix(libs/user - typo)

--- src/libs/user.js
+++ src/libs/user.js
@@ -3,26 +3,26 @@ import { grabAndSaveImage } from '@libs/files';
 import { redis } from '@app';
 import { USERS } from '@constants/keys';
 
-export const createUser = payload => new Promise(async (resolve, reject) => {
+export const createUser = payload =>
+	new Promise(async (resolve, reject) => {
+		const { avatar, socialId, social, ...data } = payload;
 
-    const { avatar, socialId, socia, ...data } = payload;
+		let user = Object.assign({}, data);
 
-    let user = Object.assign({}, data);
+		try {
+			if (avatar) {
+				user.avatar = await grabAndSaveImage({ url: avatar, type: 'avatars' });
+			}
 
-    try {
-        if (avatar) {
-            user.avatar = await grabAndSaveImage({ url: avatar, type: 'avatars' })
-        }
+			if (socialId) {
+				user[social] = socialId;
+			}
 
-        if (socialId) {
-            user[social] = socialId;
-        }
+			user = await new User(user).save();
+			await redis.lpush(USERS, user._id);
 
-        user = await new User(user).save();
-        await redis.lpush(USERS, user._id);
-
-        return resolve(user.toObject());
-    } catch (error) {
-        return reject(error);
-    }
-})
+			return resolve(user.toObject());
+		} catch (error) {
+			return reject(error);
+		}
+	});
