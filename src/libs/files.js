2c3a1ccab778bea290eb54287fc5f7c705a3dab2
Artem Gordeev <gordeevartem.dev@gmail.com>
2017-10-06 17:06:05 +0300

    fix(libs/files - naming)

--- src/libs/files.js
+++ src/libs/files.js
@@ -5,37 +5,36 @@ import sharp from 'sharp';
 import { paths } from '@config';
 
 const previewResolutionByType = {
-    avatars: [150, 150],
-    messages: [200, 200]
+	avatars: [150, 150],
+	messages: [200, 200]
 };
 
 export const grabAndSaveImage = ({ url, type }) => {
+	const random = randomstring.generate();
+	const filename = `${random}.png`;
 
-    const random = randomstring.generate();
-    const filename = `${random}.png`;
+	const req = request.get(url);
+	const resolution = previewResolutionByType[type];
+	const resizer = sharp()
+		.resize(...resolution)
+		.min();
+	const originWriter = fs.createWriteStream(`${paths.static_dir}/${filename}`);
+	const previewWriter = fs.createWriteStream(`${paths.static_dir}/preview_${filename}`);
 
-    const request = request.get(url);
-    const resolution = previewResolutionByType[type];
-    const resizer = sharp().resize(...resolution).min();
-    const originWriter = fs.createWriteStream(`${paths.static_dir}/${filename}`);
-    const previewWriter = fs.createWriteStream(`${paths.static_dir}/preview_${filename}`);
+	const origin = new Promise((resolve, reject) =>
+		req
+			.pipe(originWriter)
+			.on('error', reject)
+			.on('close', () => resolve(filename))
+	);
 
-    const origin = new Promise((resolve, reject) =>
-        request
-            .pipe(originWriter)
-            .on('error', reject)
-            .on('close', () => resolve(filename))
-    );
+	const preview = new Promise((resolve, reject) =>
+		req
+			.pipe(resizer)
+			.pipe(previewWriter)
+			.on('error', reject)
+			.on('close', () => resolve(filename))
+	);
 
-    const preview = new Promise((resolve, reject) =>
-        request
-            .pipe(resizer)
-            .pipe(previewWriter)
-            .on('error', reject)
-            .on('close', () => resolve(filename))
-    );
-
-    return Promise
-        .all([origin, preview])
-        .then(result => result[0]);
+	return Promise.all([origin, preview]).then(result => result[0]);
 };
